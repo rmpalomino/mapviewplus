6f2af0f326c8a7e6201326b3bbf6a3aa2da54578
rmpalomino <Richard.m.palomino.15@dartmouth.edu>
2015-08-28 16:45:00 -0500

    Committing initial release

--- src/MapViewPlus.cs
+++ src/MapViewPlus.cs
@@ -5,42 +5,54 @@ using KSP;
 
 namespace MapViewPlus
 {
-	[KSPAddon (KSPAddon.Startup.MainMenu, true)]
+	[KSPAddon (KSPAddon.Startup.Flight, false)]
 	public class MapViewPlus : MonoBehaviour
 	{
 		public static ApplicationLauncherButton MVPButton = null;
 
 		public bool showGUI;
 		public Rect windowRect;
+		public Vector2 scrollPos;
 		public System.Collections.Generic.List<MapObject> objects;
 
 		PlanetariumCamera pCamera;
 
 		public void Awake(){
-			GameObject.DontDestroyOnLoad (this.gameObject);
+
 			showGUI = false;
 			pCamera = PlanetariumCamera.fetch;
-			windowRect = new Rect (20, 20, 200, 500);
+			windowRect = new Rect (20, 40, 200, 500);
+			scrollPos = new Vector2 (0, 0);
 			objects = pCamera.targets;
-
 			#if DEBUG
 			Debug.Log ("[MVP] Registering Callback");
-			#endif
-
 			Debug.Log ("[MVP] Called awake");
+			Debug.Log("[MVP] listing the targets and indices");
+			for(int i = 0; i < objects.Count; i++)
+				Debug.Log("[MVP] " + objects[i].name + " has index " + i);
+			#endif
 
 			//Prepare GUI and ApplicationLauncherButton
 			if (MVPButton == null) {
-
+				#if DEBUG
 				Debug.Log ("[MVP] Passed null check");
-				GameEvents.onGUIApplicationLauncherReady.Add(AddAppButton);
-				GameEvents.onGUIApplicationLauncherDestroyed.Add(RemoveAppButton);
+				#endif
 			}
 		}
 
 		public void Start ()
 		{
+			AddAppButton ();
 			MapView.OnEnterMapView += SetUpMapView;
+			MapView.OnExitMapView += DestroyAddon;
+		}
+
+		public void OnDestroy(){
+			RemoveAppButton ();
+		
+			#if DEBUG
+			Debug.Log("[MVP] Calling OnDestroy");
+			#endif
 		}
 
 
@@ -61,36 +73,75 @@ namespace MapViewPlus
 					ApplicationLauncher.AppScenes.MAPVIEW, 
 					(Texture)GameDatabase.Instance.GetTexture ("MVP/Texture/StockIcon", false));
 
+				#if DEBUG
 				Debug.Log ("[MVP] Checking button address " + MVPButton);
 				Debug.Log ("[MVP] Created a button");
+				#endif
 			}
 		}
 
 		public void RemoveAppButton() {
-			//ApplicationLauncher.Instance.RemoveApplication (MVPButton);
+			ApplicationLauncher.Instance.RemoveModApplication (MVPButton);
+			MapViewPlus.MVPButton = null;
+			#if DEBUG
 			Debug.Log ("[MVP] Just called to remove button");
+			#endif
 		}
 
 		void OnGUI(){
 			if (this.showGUI) {
-				Debug.Log ("Trying to draw :(");
-				windowRect = GUILayout.Window (03755, windowRect, this.SelectionWindow, "Select Focus");
+				if (HighLogic.LoadedScene == GameScenes.FLIGHT && MapView.MapIsEnabled)
+					windowRect = GUILayout.Window (03755, windowRect, this.SelectionWindow, "Select Focus");
+				else {
+					this.showGUI = false;
+				}
 			}
 		
 		}
 
 		void SelectionWindow (int windowID) {
-			GUILayout.BeginVertical ();
-			foreach (MapObject mapObject in this.objects)
-				if (GUILayout.Button (mapObject.name))
-					pCamera.SetTarget (mapObject);
-			GUILayout.EndVertical ();
+			scrollPos = GUILayout.BeginScrollView (scrollPos,
+				GUILayout.ExpandWidth (false),
+				GUILayout.MaxHeight (GameSettings.SCREEN_RESOLUTION_HEIGHT * 0.75f)
+			);
+			int vesselI = -1;
+			for(int manueverI = this.objects.Count - 1; manueverI >= 0; manueverI--) {
+				MapObject aMapObject = this.objects [manueverI];
+
+				if (aMapObject.type == MapObject.MapObjectType.VESSEL) {
+					vesselI = manueverI;
+					if(vesselI != this.objects.Count - 1) {
+						for (int i = vesselI + 1; i < this.objects.Count; i++)
+							if (GUILayout.Button (this.objects[i].name))
+								pCamera.SetTarget (this.objects[i]);
+					}
+
+					break;
+				}
+					
+			}
+
+			for (int celestialI = 0; celestialI < vesselI; celestialI++) {
+				MapObject aMapObject = this.objects [celestialI];
+				if (aMapObject.type == MapObject.MapObjectType.MANEUVERNODE)
+					break;
+
+				if (GUILayout.Button (aMapObject.name))
+					pCamera.SetTarget (aMapObject);
+			}
+
+			if (this.objects[vesselI].type == MapObject.MapObjectType.VESSEL && GUILayout.Button ("Return to Ship"))
+				pCamera.SetTarget (this.objects [vesselI]);
+
+			GUILayout.EndScrollView();
 			GUI.DragWindow ();
 		}
 
 		public void SetUpMapView(){
 			#if DEBUG
 			Debug.Log ("[MVP] Auto navball toggle");
+			for(int i = 0; i < objects.Count; i++)
+				Debug.Log("[MVP] " + objects[i].name + " has index " + i);
 			#endif
 
 			//Auto expand the navball
@@ -107,16 +158,17 @@ namespace MapViewPlus
 			}
 		}
 
+		public void DestroyAddon() {
+			GameObject.Destroy (this);
+		}
+
 		void OnAppLaunchTrue ()
 		{
-			Debug.Log ("[MVP] Called onTrue but no GUI?");
 			this.showGUI = true;
-			Debug.Log ("[MVP] Should have GUI, check bool: " + this.showGUI + "; check position of rect: " + windowRect.position.ToString());
 		}
 
 		void OnAppLauncherFalse ()
 		{
-			Debug.Log ("[MVP] Called onFalse but no GUI?");
 			this.showGUI = false;
 		}
 
