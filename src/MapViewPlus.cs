1b0bf6660f9d7731b5e172868945f80850d7abdf
rmpalomino <Richard.m.palomino.15@dartmouth.edu>
2015-08-29 18:35:12 -0500

    Hotfixed the navball and toolbar icon, added more comments

--- src/MapViewPlus.cs
+++ src/MapViewPlus.cs
@@ -17,48 +17,56 @@ namespace MapViewPlus
 
 		PlanetariumCamera pCamera;
 
-		public void Awake(){
+		#region Unity Lifecycle
+		public void Awake() {
 
+			//Initialize variables for GUI
 			showGUI = false;
-			pCamera = PlanetariumCamera.fetch;
-			windowRect = new Rect (20, 40, 200, 500);
+			windowRect = new Rect (20, 40, 200, 500); //Under Timewarp/Clock
 			scrollPos = new Vector2 (0, 0);
+
+			//Get Planetarium's MapObject information (Vessel, bodies, MNodes)
+			pCamera = PlanetariumCamera.fetch;
 			objects = pCamera.targets;
+
 			#if DEBUG
+			//Log all start up info, listing MO's before entering MapView for comparison
 			Debug.Log ("[MVP] Registering Callback");
 			Debug.Log ("[MVP] Called awake");
-			Debug.Log("[MVP] listing the targets and indices");
+			Debug.Log("[MVP] listing the targets and indices before Map View");
 			for(int i = 0; i < objects.Count; i++)
 				Debug.Log("[MVP] " + objects[i].name + " has index " + i);
 			#endif
+		}
 
-			//Prepare GUI and ApplicationLauncherButton
+		public void Start ()
+		{
+			//Make sure a button isn't hanging around when starting
 			if (MVPButton == null) {
 				#if DEBUG
 				Debug.Log ("[MVP] Passed null check");
 				#endif
 			}
-		}
 
-		public void Start ()
-		{
+			//Registers new button when Addon starts up in Flight scene
 			AddAppButton ();
+
+			//Prepares auto target
 			MapView.OnEnterMapView += SetUpMapView;
-			MapView.OnExitMapView += DestroyAddon;
 		}
 
 		public void OnDestroy(){
 			RemoveAppButton ();
-		
 			#if DEBUG
 			Debug.Log("[MVP] Calling OnDestroy");
 			#endif
 		}
+		#endregion
 
-
+		#region Stock Toolbar
 		public void AddAppButton ()
 		{
-			if (ApplicationLauncher.Ready && MVPButton == null) {
+			if (MVPButton == null) {
 				#if DEBUG
 				Debug.Log ("[MVP] AppLauncher was ready, attempting button");
 				#endif
@@ -71,7 +79,7 @@ namespace MapViewPlus
 					this.OnAppLauncherNull,
 					this.OnAppLauncherNull,
 					ApplicationLauncher.AppScenes.MAPVIEW, 
-					(Texture)GameDatabase.Instance.GetTexture ("MVP/Texture/StockIcon", false));
+					(Texture)GameDatabase.Instance.GetTexture ("MapViewPlus/Texture/StockIcon", false));
 
 				#if DEBUG
 				Debug.Log ("[MVP] Checking button address " + MVPButton);
@@ -79,87 +87,14 @@ namespace MapViewPlus
 				#endif
 			}
 		}
-
+		#endregion
+			
 		public void RemoveAppButton() {
 			ApplicationLauncher.Instance.RemoveModApplication (MVPButton);
 			MapViewPlus.MVPButton = null;
 			#if DEBUG
 			Debug.Log ("[MVP] Just called to remove button");
 			#endif
-		}
-
-		void OnGUI(){
-			if (this.showGUI) {
-				if (HighLogic.LoadedScene == GameScenes.FLIGHT && MapView.MapIsEnabled)
-					windowRect = GUILayout.Window (03755, windowRect, this.SelectionWindow, "Select Focus");
-				else {
-					this.showGUI = false;
-				}
-			}
-		
-		}
-
-		void SelectionWindow (int windowID) {
-			scrollPos = GUILayout.BeginScrollView (scrollPos,
-				GUILayout.ExpandWidth (false),
-				GUILayout.MaxHeight (GameSettings.SCREEN_RESOLUTION_HEIGHT * 0.75f)
-			);
-			int vesselI = -1;
-			for(int manueverI = this.objects.Count - 1; manueverI >= 0; manueverI--) {
-				MapObject aMapObject = this.objects [manueverI];
-
-				if (aMapObject.type == MapObject.MapObjectType.VESSEL) {
-					vesselI = manueverI;
-					if(vesselI != this.objects.Count - 1) {
-						for (int i = vesselI + 1; i < this.objects.Count; i++)
-							if (GUILayout.Button (this.objects[i].name))
-								pCamera.SetTarget (this.objects[i]);
-					}
-
-					break;
-				}
-					
-			}
-
-			for (int celestialI = 0; celestialI < vesselI; celestialI++) {
-				MapObject aMapObject = this.objects [celestialI];
-				if (aMapObject.type == MapObject.MapObjectType.MANEUVERNODE)
-					break;
-
-				if (GUILayout.Button (aMapObject.name))
-					pCamera.SetTarget (aMapObject);
-			}
-
-			if (this.objects[vesselI].type == MapObject.MapObjectType.VESSEL && GUILayout.Button ("Return to Ship"))
-				pCamera.SetTarget (this.objects [vesselI]);
-
-			GUILayout.EndScrollView();
-			GUI.DragWindow ();
-		}
-
-		public void SetUpMapView(){
-			#if DEBUG
-			Debug.Log ("[MVP] Auto navball toggle");
-			for(int i = 0; i < objects.Count; i++)
-				Debug.Log("[MVP] " + objects[i].name + " has index " + i);
-			#endif
-
-			//Auto expand the navball
-			try {
-				FlightUIModeController.Instance.navBall.Expand ();
-				MapView.fetch.MapCollapse_navBall.Expand ();
-			}
-
-			catch (Exception e) {
-				#if DEBUG
-				Debug.Log ("NavBall unaccessible");
-				Debug.Log (e.ToString ());
-				#endif
-			}
-		}
-
-		public void DestroyAddon() {
-			GameObject.Destroy (this);
 		}
 
 		void OnAppLaunchTrue ()
@@ -173,6 +108,85 @@ namespace MapViewPlus
 		}
 
 		void OnAppLauncherNull () {}
+
+		#region GUI
+		void OnGUI(){
+			if (this.showGUI) {
+				if (HighLogic.LoadedScene == GameScenes.FLIGHT && MapView.MapIsEnabled)
+					windowRect = GUILayout.Window (03755, windowRect, this.SelectionWindow, "Select Focus");
+				else {
+					this.showGUI = false;
+				}
+			}
+		
+		}
+
+		void SelectionWindow (int windowID) {
+			//Feed back the scrollbar's position
+			scrollPos = GUILayout.BeginScrollView (scrollPos,
+				GUILayout.ExpandWidth (false),
+				GUILayout.MaxHeight (GameSettings.SCREEN_RESOLUTION_HEIGHT * 0.75f)
+			);
+
+			//Find the active vessel, currently the divider
+			int vesselI = -1;
+			for(int manueverI = this.objects.Count - 1; manueverI >= 0; manueverI--) {
+				MapObject aMapObject = this.objects [manueverI];
+
+				if (aMapObject.type == MapObject.MapObjectType.VESSEL) {
+					vesselI = manueverI;
+					//Print everything after vessel (MNodes)
+					if(vesselI != this.objects.Count - 1) {
+						for (int i = vesselI + 1; i < this.objects.Count; i++)
+							if (GUILayout.Button (this.objects[i].name))
+								pCamera.SetTarget (this.objects[i]);
+					}
+
+					break;
+				}
+					
+			}
+
+			//Print everything before the divider (Celestial bodies)
+			for (int celestialI = 0; celestialI < vesselI; celestialI++) {
+				MapObject aMapObject = this.objects [celestialI];
+				if (aMapObject.type == MapObject.MapObjectType.MANEUVERNODE)
+					break;
+
+				if (GUILayout.Button (aMapObject.name))
+					pCamera.SetTarget (aMapObject);
+			}
+
+			//Print the vessel
+			if (this.objects[vesselI].type == MapObject.MapObjectType.VESSEL && GUILayout.Button ("Return to Ship"))
+				pCamera.SetTarget (this.objects [vesselI]);
+
+			//End GUI code
+			GUILayout.EndScrollView();
+			GUI.DragWindow ();
+		}
+
+		public void SetUpMapView(){
+			#if DEBUG
+			//Print the MO's again in-case they've updated
+			Debug.Log ("[MVP] Auto navball toggle");
+			for(int i = 0; i < objects.Count; i++)
+				Debug.Log("[MVP] " + objects[i].name + " has index " + i);
+			#endif
+				
+			//Auto expand the navball and activate it
+			if (!MapView.ManeuverModeActive) {
+
+				FlightUIModeController.Instance.navBall.Expand ();
+				MapView.fetch.MapCollapse_navBall.Expand ();
+				MapView.fetch.gameObject.SendMessage ("ToggleManeuverMode", SendMessageOptions.RequireReceiver);
+
+			}
+
+
+		}
+
+		#endregion
 	}
 }
 
